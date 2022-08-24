828d68f86f950568b14231ad63dea7d74cfc775f
Santiago Ospina Osorio <santiago.ospina@sofka.local>
2022-08-24 09:51:30 -0500

    Se crea la estructura del proyecto

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../interfaces/IProduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private url = 'http://localhost:3000/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.url);
+  }
+}
